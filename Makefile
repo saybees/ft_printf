f9e6ac6e4ff1529334299354e6e05e0bbec7feaa
Saydee Brown <sabrown@f1br6s1.clusters.42paris.fr>
2025-05-21 23:52:33 +0200

    first commit

---
+++ Makefile
@@ -0,0 +1,37 @@
+NAME = libft.a
+
+AR = ar
+ARFLAGS = crs
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+
+RM = rm
+RMFLAG = -f
+
+INCLUDES =
+
+SRCS	=
+
+OBJS = $(SRCS:.c=.o)
+
+%.o: %.c $(INCLUDES)
+		$(CC) $(CFLAGS) -c $< -o $@
+
+all : $(NAME)
+
+$(NAME): $(OBJS)
+				$(AR) $(ARFLAGS) $(NAME) $(OBJS)				
+
+clean :
+		$(RM) $(RMFLAG) $(OBJS)
+
+fclean : clean
+		$(RM) $(RMFLAG) $(NAME)
+
+re : fclean all
+
+bonus : $(OBJS)
+		$(AR) $(ARFLAGS) $(NAME) $(OBJS)
+
+.PHONY : all clean fclean re bonus
---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabrown <sabrown@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 21:55:56 by sabrown           #+#    #+#             */
+/*   Updated: 2025/05/21 22:08:39 by sabrown          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdarg.h>
+
+
+#endif
